b4189324fe0a7b0926ffea625e30820d438cac88
Lynelle Herrera <lcamp118@live.com>
2024-03-19 23:03:59 -0500

    implement react navigation stack

--- App.js
+++ App.js
@@ -1,9 +1,40 @@
-import React, { useState, useRef } from 'react'
-import GeneralJokes from './src/components/joke/generalJokes'
-import ProgrammingJokes from './src/components/joke/programmingJokes'
+import React from 'react'
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {Button, View, Text} from 'react-native';
+import ProgrammingJokes from './src/components/joke/programmingJokes';
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator();
+
+  function HomeScreen() {
+
+    const handlePress = () => {
+      console.log('test')
+    }
+    return (
+<View>
+<Text>Well, hello there!</Text>
+<Text>I am JokeBot.</Text>
+<Text>Would you like to hear a joke?</Text>
+
+<Button title={'TELL ME A JOKE!'} color={'white'} onPress={handlePress}></Button>
+</View>
+    );
+  }
+  
+
   return (
-    <GeneralJokes />
+    <NavigationContainer>
+<Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'Welcome'}}
+        />
+        {/* <Stack.Screen name="Profile" component={ProgrammingJokes} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
